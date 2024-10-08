78e9c3ea662495b2815d409ef5833e7ac647b6e5
Ryan Dinh <rqd3qmk@virginia.edu>
2024-10-08 15:48:40 -0400

    1525. Number of Good Ways to Split a String

---
+++ google practice/1525. Number of Good Ways to Split a String.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numSplits(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        leftCounts = [0] * len(s)
+        rightCounts = [0]* len(s)
+
+        leftSeen = set()
+        rightSeen = set()
+
+        for i in range(len(s)):
+            leftSeen.add(s[i])
+            leftCounts[i] = len(leftSeen)
+
+        for i in range(len(s) - 1, -1, -1): #going in reverse
+           rightSeen.add(s[i])
+           rightCounts[i] = len(rightSeen)
+
+        splits = 0
+        for i in range(len(s) - 1):
+            if leftCounts[i] == rightCounts[i+1]:
+                splits +=1
+        
+        return splits
+        
